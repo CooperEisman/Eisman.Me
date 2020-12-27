eb70165edcd98a8b1d58454d4e0be2d7386e97c2
Cooper Eisman <cooper@eisman.me>
2020-12-27 04:47:20 +0000

    Added NaviBar - First Steps

--- script.js
+++ script.js
@@ -14,3 +14,12 @@ switcher.addEventListener('click', function() {
     }
 	console.log('current class name: ' + className);
 });
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
